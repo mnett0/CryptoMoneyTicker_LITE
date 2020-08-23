c0c80169c7d436b8999ddec8d0d852253906b364
Mederic <medericnetto@gmail.com>
2020-08-23 22:07:49 +0200

    Update API, WiFi connect...

--- src/main.ino
+++ src/main.ino
@@ -32,9 +32,8 @@
 #include <ArduinoJson.h>
 
 #include <ESP8266WiFi.h>
-#include <DNSServer.h>
-#include <ESP8266WebServer.h>
-#include <WiFiClientSecure.h>
+#include <WiFiClientSecureBearSSL.h>
+#include <ESP8266HTTPClient.h>
 
 #include <AutoConnect.h>
 
@@ -47,45 +46,48 @@
 
 U8X8_SH1106_128X64_NONAME_HW_I2C u8x8(U8X8_PIN_NONE, 4, 5);
 
-#define BUTTON_A 14 
-#define BUTTON_B 12 
-#define BUTTON_C 13  
+#define BUTTON_A 14
+#define BUTTON_B 12
+#define BUTTON_C 13
 
-const char host[] = "api.coinpaprika.com";
-const int httpsPort = 443;
+// const char host[] = "api.coinpaprika.com";
+// const int httpPort = 80;
+// Expiration le jeudi 23 septembre 2021 à 12:54:21 heure d’été d’Europe centrale
+const uint8_t fingerprint[20] = {0x76, 0xc0, 0x35, 0xe4, 0xe7, 0x7e, 0x0e, 0xab, 0x61, 0xcf, 0xe9, 0xcd, 0x5a, 0x3e, 0x3f, 0x68, 0x9b, 0xc8, 0x9f, 0xcb};
 
 unsigned long previousMillis = 0;
 long interval = 0;
 
 #define NUM_BUTTONS 3
 const uint8_t BUTTON_PINS[NUM_BUTTONS] = {BUTTON_A, BUTTON_B, BUTTON_C};
-Bounce * buttons = new Bounce[NUM_BUTTONS];
+Bounce *buttons = new Bounce[NUM_BUTTONS];
 
-bool btnState1      = LOW;
-bool btnState2      = LOW;
-bool btnState3      = LOW;
+bool btnState1 = LOW;
+bool btnState2 = LOW;
+bool btnState3 = LOW;
 bool antiFlickering = LOW;
 
-// https://api.coinmarketcap.com/v2/listings/ for find the {id} of the currency
-// Change the name of the currency and put the {id} in " "
-#define BTC     "btc-bitcoin"
-#define ETH     "eth-ethereum"
-#define XRP     "xrp-xrp"
-#define USDT    "usdt-tether" 
-#define BCH     "bch-bitcoin-cash" 
+// For choose ur cryptocurrency -> https://api.coinpaprika.com/v1/coins <-
+#define ETH "eth-ethereum"
+#define VET "vet-vechain"
+#define OCEAN "ocean-ocean-protocol"
+#define EWT "ewt-energy-web-token"
+#define ENJ "enj-enjin-coin"
+// #define LINK "link-chainlink"
 
 // and change again the name here
-String crypto[] = {BTC, ETH, XRP, USDT, BCH};
+String crypto[] = {ETH, VET, OCEAN, EWT, ENJ};
 int coin = -1;
 String oldPrice[5];
 
-ESP8266WebServer Server;  
-AutoConnect      Portal(Server);
+ESP8266WebServer Server;
+AutoConnect Portal(Server);
 
-void setup() {
+void setup()
+{
 
   Serial.begin(115200);
-  
+
   u8x8.begin();
   u8x8.setContrast(10);
   u8x8.setFont(u8x8_font_chroma48medium8_r);
@@ -95,104 +97,106 @@ void setup() {
   //u8x8.drawString(5, 4, "v1.0");
   u8x8.drawString(2, 7, "git.io/fxRYm");
 
- //WiFiManager wifiManager;
- // wifiManager.autoConnect("TTGO-CONFIG", "12345678");
- 
-
-  for (int i = 0; i < NUM_BUTTONS; i++) {
-    buttons[i].attach(BUTTON_PINS[i] , INPUT_PULLUP);  //setup the bounce instance for the current button
-    buttons[i].interval(25);  // interval in ms
+  for (int i = 0; i < NUM_BUTTONS; i++)
+  {
+    buttons[i].attach(BUTTON_PINS[i], INPUT_PULLUP); //setup the bounce instance for the current button
+    buttons[i].interval(25);                         // interval in ms
   }
 
   Portal.begin();
-  Portal.handleClient();
 }
 
-void loop() {
-  
+void loop()
+{
+
+  Portal.handleClient();
+
   buttonCheck();
 
   //unsigned long currentMillis = millis();
 
-  if (btnState2 == HIGH || btnState3 == HIGH || (millis() - previousMillis) > interval) {
+  if (btnState2 == HIGH || btnState3 == HIGH || (millis() - previousMillis) > interval)
+  {
     previousMillis = millis();
     interval = 60000;
 
-    if(btnState2 == LOW && btnState3 == LOW && btnState1 == LOW){
-     coin++;
-    }else{
-     antiFlickering = HIGH;
+    if (btnState2 == LOW && btnState3 == LOW && btnState1 == LOW)
+    {
+      coin++;
+    }
+    else
+    {
+      antiFlickering = HIGH;
     }
 
-    if(coin > 4) {
-     coin = 0;
-    }else if(coin < 0){
+    if (coin > 4)
+    {
+      coin = 0;
+    }
+    else if (coin < 0)
+    {
       coin = 4;
     }
 
-    //Serial.print(">>> Connecting to ");
-    //Serial.println(host);
+    String payload;
 
-    WiFiClientSecure client;
+    std::unique_ptr<BearSSL::WiFiClientSecure> client(new BearSSL::WiFiClientSecure);
 
-    if (!client.connect(host, httpsPort)) {
-    u8x8.clear();
-    u8x8.drawString(0, 1, "Connection failed");
-    return;
+    client->setFingerprint(fingerprint);
+
+    HTTPClient https;
+
+    if (https.begin(*client, "https://api.coinpaprika.com/v1/tickers/" + crypto[coin]))
+    { // HTTPS
+
+      // start connection and send HTTP header
+      int httpCode = https.GET();
+
+      // httpCode will be negative on error
+      if (httpCode > 0)
+      {
+        // HTTP header has been send and Server response header has been handled
+        Serial.printf("[HTTPS] GET... code: %d\n", httpCode);
+
+        // file found at server
+        if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY)
+        {
+          payload = https.getString();
+        }
+      }
+      else
+      {
+        Serial.printf("[HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
+      }
+
+      https.end();
+    }
+    else
+    {
+      Serial.printf("[HTTPS] Unable to connect\n");
     }
 
-    //Serial.print("Requesting URL: ");
-    //Serial.println("Connected to server!");
-    client.println("GET /v1/ticker/" + crypto[coin] + "/ HTTP/1.1");
-    client.println("Host: api.coinpaprika.com");
-    client.println("Connection: close");
-    client.println();
+    // String jsonData;
+    Serial.println(payload);
+    const size_t capacity = JSON_OBJECT_SIZE(1) + JSON_OBJECT_SIZE(10) + JSON_OBJECT_SIZE(17) + 900;
+    DynamicJsonDocument doc(capacity);
+    DeserializationError error = deserializeJson(doc, payload);
 
-   unsigned long timeout = millis();
-   while (client.available() == 0) {
-     if (millis() - timeout > 5000) {
-       u8x8.clear();
-       u8x8.drawString(0, 1, "Client Timeout!");
-       client.stop();
-       return;
-     }
+    if (error)
+    {
+      Serial.print(F("deserializeJson() failed: "));
+      Serial.println(error.c_str());
+      return;
     }
 
-    String data;
-    while(client.available()) {
-     data = client.readStringUntil('\r');
-     Serial.println(data);
-    }
+    // String name = doc["name"];     // "Dai"
+    String symbol = doc["symbol"]; // "BTC"
+    // String error = doc["error"];   // id not found
 
-    data.replace('[', ' ');
-    data.replace(']', ' ');
-
-    char buffer[data.length() + 1];
-    data.toCharArray(buffer, sizeof(buffer));
-    buffer[data.length() + 1] = '\0';
-
-    const size_t bufferSize = JSON_OBJECT_SIZE(21) + 400;
-    DynamicJsonBuffer jsonBuffer(bufferSize);
-
-    JsonObject& root = jsonBuffer.parseObject(buffer);
-
-    if (!root.success()) {
-     u8x8.clear();
-     u8x8.drawString(0, 1, "parseObject() failed");
-     return;
-    }
-
-    JsonObject& data0 = root["data"];
-    String name = data0["name"];     // "Bitcoin"
-    String symbol = data0["symbol"]; // "BTC"
-
-    JsonObject& data1 = data0["quotes"]["USD"];
-    String price = data1["price"];                          // "573.137"
-    String percent_change_1h = data1["percent_change_1h"];  // "0.04"
-    String last_updated = data0["last_updated"];            // "1472762067" <-- Unix Time Stamp
-    String error = root["error"];   // id not found 
-
-    client.stop();
+    JsonObject quotes_USD = doc["quotes"]["USD"];
+    String price = quotes_USD["price"];                         // "573.137"
+    String percent_change_1h = quotes_USD["percent_change_1h"]; // "0.04"
+    // String last_updated = quotes_USD["last_updated"];           // "1472762067" <-- Unix Time Stamp
 
     printTransition();
     printName(symbol);
@@ -203,76 +207,106 @@ void loop() {
 
     btnState2 = LOW;
     btnState3 = LOW;
-
   }
   printPagination();
 }
 
-
-void printName(String symbol) {
+void printName(String symbol)
+{
   int xSymbol;
-  if(symbol.length() == 2){
-      xSymbol = 6;
-  }else if(symbol.length() == 3){
-      xSymbol = 5;
-  }else if(symbol.length() == 4){
-      xSymbol = 4;
-  }else if(symbol.length() == 5){
-      xSymbol = 3;
+  if (symbol.length() == 2)
+  {
+    xSymbol = 6;
+  }
+  else if (symbol.length() == 3)
+  {
+    xSymbol = 5;
+  }
+  else if (symbol.length() == 4)
+  {
+    xSymbol = 4;
+  }
+  else if (symbol.length() == 5)
+  {
+    xSymbol = 3;
   }
   u8x8.draw2x2String(xSymbol, 1, symbol.c_str());
 }
 
-void printPrice(String price) {
- 
+void printPrice(String price)
+{
+
   int reduceDecimalPrice = price.toInt();
   String printPriceConvert;
   int xPrice;
 
-  if(reduceDecimalPrice >= 1000 && reduceDecimalPrice < 100000){
-   printPriceConvert = price.substring(0, 7);
-   xPrice = 5;
-  }else if(reduceDecimalPrice >= 100){
-   printPriceConvert = price.substring(0, 6);
-   xPrice = 5;
-  }else if(reduceDecimalPrice >= 10){
-   printPriceConvert = price.substring(0, 5);
-   xPrice = 5;
-  }else if(reduceDecimalPrice >= 1){
-   printPriceConvert = price.substring(0, 4);
-   xPrice = 7;
-  }else{
-   printPriceConvert = price.substring(0, 8);
-   xPrice = 4;
+  if (reduceDecimalPrice >= 1000 && reduceDecimalPrice < 100000)
+  {
+    printPriceConvert = price.substring(0, 7);
+    xPrice = 5;
+  }
+  else if (reduceDecimalPrice >= 100)
+  {
+    printPriceConvert = price.substring(0, 6);
+    xPrice = 5;
+  }
+  else if (reduceDecimalPrice >= 10)
+  {
+    printPriceConvert = price.substring(0, 5);
+    xPrice = 5;
+  }
+  else if (reduceDecimalPrice >= 1)
+  {
+    printPriceConvert = price.substring(0, 4);
+    xPrice = 7;
+  }
+  else
+  {
+    printPriceConvert = price.substring(0, 8);
+    xPrice = 4;
   }
   u8x8.draw1x2String(xPrice - 1, 4, "$");
   u8x8.draw1x2String(xPrice, 4, printPriceConvert.c_str());
 
   int xBitmapPrice = printPriceConvert.length();
-  
-  if(price != oldPrice[coin]){
-    if(price > oldPrice[coin]){
+
+  if (price != oldPrice[coin])
+  {
+    if (price > oldPrice[coin])
+    {
       u8x8.drawTile(xPrice + xBitmapPrice + 1, 4, 1, flecheHaut);
       u8x8.drawTile(xPrice + xBitmapPrice + 1, 5, 1, flecheHaut1);
-    }else{
+    }
+    else
+    {
       u8x8.drawTile(xPrice + xBitmapPrice + 1, 4, 1, flecheBas1);
       u8x8.drawTile(xPrice + xBitmapPrice + 1, 5, 1, flecheBas);
     }
   }
 }
 
-void printPagination() { 
-   if(coin == 0){
+void printPagination()
+{
+  if (coin == 0)
+  {
     u8x8.drawTile(6, 7, 4, p1);
-   }else if(coin == 1){
+  }
+  else if (coin == 1)
+  {
     u8x8.drawTile(6, 7, 4, p2);
-   }else if(coin == 2){
+  }
+  else if (coin == 2)
+  {
     u8x8.drawTile(6, 7, 4, p3);
-   }else if(coin == 3){
+  }
+  else if (coin == 3)
+  {
     u8x8.drawTile(6, 7, 4, p4);
-   }else if(coin == 4){
+  }
+  else if (coin == 4)
+  {
     u8x8.drawTile(6, 7, 4, p5);
-   }
+  }
 }
 
 /*
@@ -285,32 +319,45 @@ void printError(String error) {
 }
 */
 
-void printTransition(){
-    delay(100);
-    u8x8.clear();
+void printTransition()
+{
+  delay(100);
+  u8x8.clear();
 }
 
-void buttonCheck() {
+void buttonCheck()
+{
 
-  for (int i = 0; i < NUM_BUTTONS; i++)  {
+  for (int i = 0; i < NUM_BUTTONS; i++)
+  {
     // Update the Bounce instance :
     buttons[i].update();
-    if ( buttons[i].fell() && i == 0) {
+    if (buttons[i].fell() && i == 0)
+    {
       btnState1 = !btnState1;
       //antiFlickering = HIGH;
       //tft.fillRect(95, 297, 47, 7, CUSTOM_DARK);
       //tft.fillRect(111, 287, 15, 20, CUSTOM_DARK);
-      Serial.print("Bouton 1 est préssé "); Serial.println(btnState1);
-    }else if(buttons[i].fell() && i == 1) {
+      Serial.print("Bouton 1 est préssé ");
+      Serial.println(btnState1);
+    }
+    else if (buttons[i].fell() && i == 1)
+    {
       btnState2 = HIGH;
-      Serial.print("Bouton 2 est préssé "); Serial.println(btnState2);
-       if(btnState2 == HIGH){
-        coin--;  
+      Serial.print("Bouton 2 est préssé ");
+      Serial.println(btnState2);
+      if (btnState2 == HIGH)
+      {
+        coin--;
       }
-    }else if(buttons[i].fell() && i == 2) {
+    }
+    else if (buttons[i].fell() && i == 2)
+    {
       btnState3 = HIGH;
-      Serial.print("Button 3 est préssé "); Serial.println(btnState3);
-       if(btnState3 == HIGH){
+      Serial.print("Button 3 est préssé ");
+      Serial.println(btnState3);
+      if (btnState3 == HIGH)
+      {
         coin++;
       }
     }
